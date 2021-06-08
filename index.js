85aba8cd9b1c33d5c989ec8b5d4840048126cf08
Viktoria <doduchenko29@gmail.com>
2021-06-08 17:11:07 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 export const squaredNumbers = () => {
   const arr = Array.from(document.querySelectorAll('.number'));
-  console.log(arr[0].dataset)
-  console.log(arr[1].dataset)
-  console.log(arr[2].dataset);
-  arr[0].dataset.squaredNumber = '25';
-  arr[1].dataset.squaredNumber = '49';
-  arr[2].dataset.squaredNumber = '9';
+
+  const arr1 = Object.values(arr[0].dataset);
+  arr[0].dataset.squaredNumber = +arr1[0] * +arr1[0];
+
+  const arr2 = Object.values(arr[1].dataset);
+  arr[1].dataset.squaredNumber = +arr2[0] * +arr2[0];
+
+  const arr3 = Object.values(arr[2].dataset);
+  arr[2].dataset.squaredNumber = +arr3[0] * +arr3[0];
 };
 
